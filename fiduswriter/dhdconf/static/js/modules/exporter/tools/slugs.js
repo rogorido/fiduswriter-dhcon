eca9dbd48353e5ea959ba451fc62925278b31457
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2026-05-14 09:39:36 +0200

    fix: Better handling of void strings in slugLastName.

--- fiduswriter/dhdconf/static/js/modules/exporter/tools/slugs.js
+++ fiduswriter/dhdconf/static/js/modules/exporter/tools/slugs.js
@@ -1,10 +1,9 @@
 export const createSlugLastName = str => {
-
-        if (str === "") {
-            str = gettext("Untitled")
-        }
-        str = str.replace(/[^a-zA-Z0-9\s]/g, "")
-       str = str.toLowerCase()
-        str = str.replace(/\s/g, "-")
-        return str.toUpperCase()
+    if (str === "" || str == undefined) {
+        str = gettext("UNKNOWN")
     }
+    str = str.replace(/[^a-zA-Z0-9\s]/g, "")
+    str = str.toLowerCase()
+    str = str.replace(/\s/g, "-")
+    return str.toUpperCase()
+}
